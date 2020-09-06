56e99b52a969658e96ead781225174e55be3fc0d
Ramses A <iocoder@aol.com>
2020-09-05 20:09:58 -0400

    Add license headings to build files.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,30 @@
+#**************************************************************************
+#*
+#*                   ARTOS Operating System.
+#*                 Copyright (C) 2020  ARMKit.
+#*
+#**************************************************************************
+#* @file   Dockerfile
+#* @brief  Docker instructions to build ARTOS using an ubuntu image.
+#**************************************************************************
+#*
+#* This program is free software; you can redistribute it and/or
+#* modify it under the terms of the GNU General Public License
+#* as published by the Free Software Foundation; either version 2
+#* of the License, or (at your option) any later version.
+#*
+#* This program is distributed in the hope that it will be useful,
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#* GNU General Public License for more details.
+#*
+#* You should have received a copy of the GNU General Public License
+#* along with this program; if not, write to the Free Software
+#* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#* 02110-1301, USA.
+#*
+#**************************************************************************
+
 FROM ubuntu:latest
 
 ENV DEBIAN_FRONTEND=noninteractive
--- build.bat
+++ build.bat
@@ -1,4 +1,32 @@
 @echo off
 echo.
+
+REM **************************************************************************
+REM *
+REM *                   ARTOS Operating System.
+REM *                 Copyright (C) 2020  ARMKit.
+REM *
+REM **************************************************************************
+REM * @file   build.bat
+REM * @brief  ARTOS build instruction, Windows NT.
+REM **************************************************************************
+REM *
+REM * This program is free software; you can redistribute it and/or
+REM * modify it under the terms of the GNU General Public License
+REM * as published by the Free Software Foundation; either version 2
+REM * of the License, or (at your option) any later version.
+REM *
+REM * This program is distributed in the hope that it will be useful,
+REM * but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM * GNU General Public License for more details.
+REM *
+REM * You should have received a copy of the GNU General Public License
+REM * along with this program; if not, write to the Free Software
+REM * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+REM * 02110-1301, USA.
+REM *
+REM **************************************************************************
+
 docker build --build-arg RANDOM=%RANDOM% -t artos:latest .
 docker run -v %CD%:/mnt -t artos:latest cp -r /artos/build /mnt
